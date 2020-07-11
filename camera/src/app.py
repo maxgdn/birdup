28e5668e8fb990dbd68917205ff56f80a7abbd17
MaxGDN <maxvdd@hotmail.com>
2020-07-11 15:28:56 -0400

    added capture sample

--- camera/src/app.py
+++ camera/src/app.py
@@ -3,6 +3,11 @@ from aiohttp import web
 async def handle(request):
     data = await request.json()
     text = "Hello, " + data['uuid']
+
+    # return 200 if image was taken
+
+    # return 500 otherwise
+
     return web.Response(text=text)
 
 app = web.Application()
---
+++ camera/src/capture.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+
+import logging
+import os
+import subprocess
+import sys
+
+import gphoto2 as gp
+
+def main():
+    logging.basicConfig(
+        format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
+    callback_obj = gp.check_result(gp.use_python_logging())
+    camera = gp.Camera()
+    camera.init()
+    print('Capturing image')
+    file_path = camera.capture(gp.GP_CAPTURE_IMAGE)
+    print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+    target = os.path.join('/tmp', file_path.name)
+    print('Copying image to', target)
+    camera_file = camera.file_get(
+        file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
+    camera_file.save(target)
+    subprocess.call(['xdg-open', target])
+    camera.exit()
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
