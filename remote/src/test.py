09eaecda3c2cfa4c269cc0f29712a2133fb4ba06
MaxGDN <maxvdd@hotmail.com>
2020-07-19 07:09:41 -0400

    test x2

--- remote/src/test.py
+++ remote/src/test.py
@@ -2,17 +2,23 @@ import bluetooth
 
 uuid = "0C:FC:83:2F:19:6D"
 
-server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-
-port = bluetooth.get_available_port( bluetooth.RFCOMM )
-server_sock.bind(("",port))
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("", bluetooth.PORT_ANY))
 server_sock.listen(1)
-print "listening on port %d" % port
 
-bluetooth.advertise_service( server_sock, "Service", uuid )
+port = server_sock.getsockname()[1]
 
-client_sock,address = server_sock.accept()
-print "Accepted connection from ",address
+
+bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                            # protocols=[bluetooth.OBEX_UUID]
+                            )
+
+print("Waiting for connection on RFCOMM channel", port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from", client_info)
 
 try:
     while True:
@@ -23,6 +29,9 @@ try:
 except OSError:
     pass
 
+print("Disconnected.")
+
 client_sock.close()
 server_sock.close()
+print("All done.")
 
