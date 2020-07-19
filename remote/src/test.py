00178745477d039db34bdb39849df584626bd05b
MaxGDN <maxvdd@hotmail.com>
2020-07-19 07:06:19 -0400

    lets test

--- remote/src/test.py
+++ remote/src/test.py
@@ -1,20 +1,28 @@
-import bluetooth 
+import bluetooth
 
-hostMACAddress = '0C:FC:83:2F:19:6D'
-port = 3 
-backlog = 1
-size = 1024
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.bind((hostMACAddress, port))
-s.listen(backlog)
+uuid = "0C:FC:83:2F:19:6D"
+
+server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
+port = bluetooth.get_available_port( bluetooth.RFCOMM )
+server_sock.bind(("",port))
+server_sock.listen(1)
+print "listening on port %d" % port
+
+bluetooth.advertise_service( server_sock, "Service", uuid )
+
+client_sock,address = server_sock.accept()
+print "Accepted connection from ",address
+
 try:
-	client, clientInfo = s.accept()
-	while 1:
-		data = client.recv(size)
-		if data:
-			print(data)
-			client.send(data) # Echo back to client
-except:	
-	print("Closing socket")
-	client.close()
-	s.close()
+    while True:
+        data = client_sock.recv(1024)
+        if not data:
+            break
+        print("Received", data)
+except OSError:
+    pass
+
+client_sock.close()
+server_sock.close()
+
